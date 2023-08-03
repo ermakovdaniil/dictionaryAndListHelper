365b3c0099db62492a803b70db4207ad1404a8a1
Danya <ermakov1709@mail.ru>
2023-08-03 14:55:23 +0300

    Добавьте файлы проекта.

---
+++ dictionaryToTxt/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class TextCreator
+{
+
+    public static void Main(string[] args)
+    {
+        // Creates templates for dictionary and list from txt file which contains values
+
+        CreateTXT("\t\t\t{ \"\", \"", "\" },", "", "");
+
+        CreateTXT("\t\t\t\"", "\",", "", "");
+    }
+
+    private static void CreateTXT(string before, string after, string inputPath, string outputPath)
+    {
+        string[] text = File.ReadAllLines(inputPath);
+
+        string temp;
+        List<string> list = new List<string>();
+        string[] dictionary;
+        var foundIndexes = new List<int>();
+        foreach (string line in text)
+        {
+            string modified = line;
+
+            if (line.ToLower().Contains('"'))
+            {
+
+                for (int i = line.IndexOf('"'); i > -1; i = line.IndexOf('"', i + 1))
+                {
+                    foundIndexes.Add(i);
+                }
+
+                for (int i = 0; i < foundIndexes.Count; i++)
+                {
+                    foundIndexes[i] += i;
+                }
+
+                foreach (int i in foundIndexes)
+                {
+                    modified = modified.Insert(i, @"\");
+                }
+                foundIndexes.Clear();
+            }
+            temp = before + modified + after;
+            list.Add(temp);
+        }
+        dictionary = list.ToArray();
+        File.WriteAllLines(outputPath, dictionary);
+    }
+}
